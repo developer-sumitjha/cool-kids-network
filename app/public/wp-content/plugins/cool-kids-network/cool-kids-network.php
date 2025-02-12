65f1f966aa43827ce41c69116948d7c67060c976
Sumit Jha <developersumit99@gmail.com>
2025-02-12 22:23:25 +0545

    Custom Plugin Created

---
+++ app/public/wp-content/plugins/cool-kids-network/cool-kids-network.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Cool Kids Network
+ * Description: Implements user management and features for the Cool Kids Network.
+ * Version: 1.0
+ * Author: Sumit Jha
+ */
+
+// Security check
+defined('ABSPATH') || exit;
